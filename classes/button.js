e595af90d6e4570defce336e96c0c435c7aebebf
erich-m <erich.m.maclean@gmail.com>
2022-01-18 15:40:48 -0500

    redesigned UI, added new button class with centered functionality, added cleaner api fetch implementation

---
+++ classes/button.js
@@ -0,0 +1,113 @@
+//Button Class to render and flash buttons on screen
+class Button {
+	constructor(x,y,w,h,r,margin,fillColor,flashColor,id,note,volume){
+		this.x = x;
+		this.y = y;
+		this.w = w-(2*margin);
+		this.h = h-(2*margin);
+		this.r = r;
+
+		this.m = margin;
+
+		this.note = note;
+		this.v = volume;
+
+		this.id = id;
+		
+		this.c = fillColor;
+		this.flashColor = color(red(fillColor)-flashColor,green(fillColor)-flashColor,blue(fillColor)-flashColor);
+		
+		this.flashStatus = false;//is flash on or off
+		this.flashTargetTime = 0;//flash to set time
+		this.buttonStatus = false;
+		this.userControl = false;
+	}
+	render(s,sW){
+		strokeWeight(sW);
+		stroke(s);
+		if(!this.flashStatus){
+			fill(this.c);
+		}else{
+			fill(this.flashColor);
+		}
+		rectMode(CENTER);
+		rect(this.x,this.y,this.w,this.h,this.r);
+	}
+	update(x,y,w,h,r,margin){
+		this.x = x;
+		this.y = y;
+		this.w = w-(2*margin);
+		this.h = h-(2*margin);
+		this.r = r;
+
+		this.m = margin;
+	}
+	flash(waitTime,flashControl,updateFunction){
+		if(this.buttonStatus){
+			if(((mouseIsPressed && this.userControl && this.checkIntersection()) || flashControl) && !this.flashStatus && this.flashTargetTime == 0){
+				if(mouseIsPressed){
+					this.flashTargetTime = millis() + (500);
+				}else{
+					this.flashTargetTime = millis() + (1000 * waitTime);
+				}
+				this.flashStatus = true;
+				//console.log(this.id);//impusle output on activation
+				this.playSynth();
+				if(typeof(updateFunction) == "function"){
+            		updateFunction(this.id);
+				}
+			}
+			if(this.flashStatus && millis() >= this.flashTargetTime){
+				this.flashStatus = false;
+				this.flashTargetTime = 0;
+			}
+		}
+	}
+	checkIntersection(){
+		if((-this.w/2+this.x <= mouseX && mouseX < (this.w/2+this.x)) && (-this.h/2+this.y <= mouseY && mouseY < (this.h/2+this.y))){//check outer bounds of button
+			//set up inner bounds
+			let topLeft = [-this.w/2+this.x-this.r,-this.h/2+this.y-this.r];//top left corner of inner bound
+			let topRight = [this.w/2+this.x-this.r,-this.h/2+this.y-this.r];//top right corner of inner bound
+			let bottomLeft = [-this.w/2+this.x-this.r,this.h/2+this.y-this.r];//bottom left of inner bound
+			let bottomRight = [this.x/2+this.x-this.r,this.y/2+this.y-this.r];//bottom right of inner bound
+			
+			if((topLeft[0] <= mouseX && mouseX <= topRight[0]) || (topLeft[1] <= mouseY && mouseY <= bottomRight[1])){//check inner bounds
+				return true;
+			}else{
+				//check radial bounds
+				if(dist(mouseX,mouseY,topLeft[0],topLeft[1]) <= this.r || dist(mouseX,mouseY,topRight[0],topRight[1]) <= this.r || dist(mouseX,mouseY,bottomLeft[0],bottomLeft[1]) <= this.r || dist(mouseX,mouseY,bottomRight[0],bottomRight[1]) <= this.r){
+					return true;
+				}else{
+					return false;
+				}
+			}
+		}else{
+			return false;
+		}
+	}
+	enableButton(){
+		this.buttonStatus = true;
+	}
+	
+	disableButton(){
+		this.buttonStatus = false;
+	}
+
+	enableUserControl(){
+		this.userControl = true;
+	}
+
+	disableUserControl(){
+		this.userControl = false;
+	}
+	playSynth(){
+		if(this.v){
+			userStartAudio();
+			monoSynth.play(this.note,this.v,0,0.25);
+		}
+	}
+	updateVolume(){
+		this.v = !this.v;
+	}
+}
+
--- main.js
+++ main.js
@@ -1,11 +1,87 @@
+let buttons = [];
+
+let monoSynth;
+
+let fetchedInfo = [];
+let previousInfo = [];
+
+let loadingGif;
+let currentImage;
+
+let margin = 70;
+
 function setup(){
 	createCanvas(windowWidth,windowHeight);
+
+	fetchApi();
+	
+	buttons[0] = new Button(windowWidth/8,windowHeight/2,windowWidth/3,windowHeight*0.75,10,margin,color('#F52E2E'),70,0,220,0.8);
+	buttons[1] = new Button(windowWidth/8*7,windowHeight/2,windowWidth/3,windowHeight*0.75,10,margin,color('#5463FF'),70,0,440,0.8);
+	for(let e = 0;e < buttons.length;e++){
+		buttons[e].enableButton();
+		buttons[e].enableUserControl();
+	}
+	getAudioContext().suspend();
+	monoSynth = new p5.MonoSynth();
+
+	loadingGif = loadImage("assets/loading.gif");
 }
 
 function draw(){
-	background(244,122,9);
+	background(155);
+	renderFeatures();
 
+	imageMode(CENTER);
+	image(loadingGif,windowWidth/2,windowHeight/2);
 
-	
+	if(fetchedInfo[0]){
+		let htmlImage = document.getElementsByTagName("img");
+		if(htmlImage.length > 0){
+			if(htmlImage[0].src != fetchedInfo[1]){
+				htmlImage[0].remove();
+				currentImage = createImg(fetchedInfo[1],fetchedInfo[2]);
+			}
+		}else{
+			currentImage = createImg(fetchedInfo[1],fetchedInfo[2]);
+		}
+		currentImage.hide();
+		// currentImage = createImg(fetchedInfo[1],fetchedInfo[2]);
+		image(currentImage,windowWidth/2,windowHeight/2,windowWidth/2,windowWidth/2);
+		noFill();
+		stroke(0);
+		strokeWeight(5);
+		rect(windowWidth/2,windowHeight/2,windowWidth/2,windowWidth/2);
+		currentImage.hide();
+	}
+}
+
+function renderFeatures(){
+	for(let i = 0;i < buttons.length;i++){
+		buttons[i].render(1,5);
+		
+		buttons[i].flash(3000,false,fetchApi);
+	}
+	buttons[0].update(windowWidth/8,windowHeight/2,windowWidth/3,windowHeight*0.75,10,margin);
+	buttons[1].update(windowWidth/8*7,windowHeight/2,windowWidth/3,windowHeight*0.75,10,margin);
+}
+
+function fetchApi(){
+	let url = "https://api.nasa.gov/planetary/apod?date=";
+	let apiKey = "eBxUdgb3ndh8ififjjN4O2phAzPODRBBkWcVviy2";
+	let currentDate = new Date();
+	url += currentDate.getFullYear() + "-" + (currentDate.getUTCMonth() + 1) + "-" + currentDate.getDate() + "&api_key=" + apiKey;
+
+	loadJSON(url,recievedData);
+	// console.log(url);
+
+}
+
+function recievedData(data){
+	fetchedInfo[0] = true;
+	fetchedInfo[1] = data.hdurl;
+	fetchedInfo[2] = data.title;
+	fetchedInfo[3] = data.explanation;
+	fetchedInfo[4] = data.copyright;
+	console.log(fetchedInfo);
 }
 
